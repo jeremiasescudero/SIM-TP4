7753447ee5990f72fcdf03350d8e0d7897d09913
Jeremias Escudero <jeremiasescudero67@gmail.com>
2024-12-01 12:39:13 -0300

    m

--- main.py
+++ main.py
@@ -14,15 +14,14 @@ app = Flask(__name__, template_folder=os.path.join(os.path.dirname(__file__), 't
 class Simulacion:
     def __init__(self, equipos=6, inf_inscripcion=5, sup_inscripcion=8, media_llegada=2.0):
         self.equipos = [{'id': i+1, 'estado': EstadoEquipo.LIBRE, 
-                        'fin_inscripcion': None,
-                        'fin_mantenimiento': None} for i in range(equipos)]
+                        'fin_inscripcion': None} for i in range(equipos)]
         self.inf_inscripcion = inf_inscripcion
         self.sup_inscripcion = sup_inscripcion
         self.media_llegada = media_llegada
         self.cola = 0
         self.tiempo_actual = 0
         self.resultados = []
-        self.proximo_mantenimiento = 60
+        self.contador_alumnos = 0
 
     def generar_tiempo_llegada(self):
         rnd = random.random()
@@ -34,11 +33,6 @@ class Simulacion:
         tiempo = self.inf_inscripcion + (self.sup_inscripcion - self.inf_inscripcion) * rnd
         return rnd, round(tiempo, 2)
 
-    def generar_tiempo_mantenimiento(self):
-        rnd = random.random()
-        tiempo = 3 + (10 - 3) * rnd
-        return rnd, round(tiempo, 2)
-
     def obtener_equipo_libre(self):
         for equipo in self.equipos:
             if equipo['estado'] == EstadoEquipo.LIBRE:
@@ -48,38 +42,16 @@ class Simulacion:
     def simular(self, tiempo_total):
         # Primera llegada
         rnd_llegada, tiempo_llegada = self.generar_tiempo_llegada()
-        self.tiempo_actual = tiempo_llegada
-        proxima_llegada = self.tiempo_actual + tiempo_llegada
-        max_iteraciones = 10000  # Límite de seguridad
-        iteracion = 0
+        proxima_llegada = tiempo_llegada
+        self.contador_alumnos += 1
+        id_actual = f"A{self.contador_alumnos}"
 
-        while self.tiempo_actual < tiempo_total and iteracion < max_iteraciones:
-            iteracion += 1
-            eventos = []
-            
-            # Recolectar todos los eventos posibles
-            eventos.append(('Llegada Alumno', proxima_llegada))
-            
-            # Verificar mantenimiento
-            if self.tiempo_actual >= self.proximo_mantenimiento:
-                eventos.append(('Inicio Mantenimiento', self.tiempo_actual))
-            
-            # Verificar fin de inscripciones y mantenimientos
-            for equipo in self.equipos:
-                if equipo['fin_inscripcion'] and equipo['fin_inscripcion'] <= proxima_llegada:
-                    eventos.append(('Fin Inscripción', equipo['fin_inscripcion'], equipo['id']))
-                if equipo['fin_mantenimiento'] and equipo['fin_mantenimiento'] <= proxima_llegada:
-                    eventos.append(('Fin Mantenimiento', equipo['fin_mantenimiento'], equipo['id']))
-
-            # Obtener el evento más próximo
-            evento = min(eventos, key=lambda x: x[1])
-            self.tiempo_actual = evento[1]
-            
+        while self.tiempo_actual < tiempo_total:
             estado = {
-                'Evento': evento[0],
+                'Evento': f'Llegada Alumno {id_actual}',
                 'Reloj': round(self.tiempo_actual, 2),
-                'RND Llegada': 'N/A',
-                'Tiempo Llegada': 'N/A',
+                'RND Llegada': round(rnd_llegada, 2),
+                'Tiempo Llegada': round(tiempo_llegada, 2),
                 'Próxima Llegada': round(proxima_llegada, 2),
                 'Máquina': 'N/A',
                 'RND Inscripción': 'N/A',
@@ -90,52 +62,21 @@ class Simulacion:
                 'Fin Mantenimiento': 'N/A'
             }
 
-            # Procesar el evento según su tipo
-            if evento[0] == 'Llegada Alumno':
+            # Procesar llegada
+            equipo_libre = self.obtener_equipo_libre()
+            if equipo_libre:
+                equipo_libre['estado'] = EstadoEquipo.OCUPADO
+                rnd_ins, tiempo_ins = self.generar_tiempo_inscripcion()
+                equipo_libre['fin_inscripcion'] = self.tiempo_actual + tiempo_ins
+                equipo_libre['alumno_actual'] = id_actual
                 estado.update({
-                    'RND Llegada': round(rnd_llegada, 2),
-                    'Tiempo Llegada': round(tiempo_llegada, 2)
+                    'Máquina': equipo_libre['id'],
+                    'RND Inscripción': round(rnd_ins, 2),
+                    'Tiempo Inscripción': round(tiempo_ins, 2),
+                    'Fin Inscripción': round(equipo_libre['fin_inscripcion'], 2)
                 })
-                equipo_libre = self.obtener_equipo_libre()
-                if equipo_libre:
-                    rnd_ins, tiempo_ins = self.generar_tiempo_inscripcion()
-                    equipo_libre['estado'] = EstadoEquipo.OCUPADO
-                    equipo_libre['fin_inscripcion'] = self.tiempo_actual + tiempo_ins
-                    estado.update({
-                        'Máquina': equipo_libre['id'],
-                        'RND Inscripción': round(rnd_ins, 2),
-                        'Tiempo Inscripción': round(tiempo_ins, 2),
-                        'Fin Inscripción': round(equipo_libre['fin_inscripcion'], 2)
-                    })
-                else:
-                    self.cola += 1
-                rnd_llegada, tiempo_llegada = self.generar_tiempo_llegada()
-                proxima_llegada = self.tiempo_actual + tiempo_llegada
-
-            elif evento[0] == 'Inicio Mantenimiento':
-                equipo_libre = self.obtener_equipo_libre()
-                if equipo_libre:
-                    rnd_mant, tiempo_mant = self.generar_tiempo_mantenimiento()
-                    equipo_libre['estado'] = EstadoEquipo.MANTENIMIENTO
-                    equipo_libre['fin_mantenimiento'] = self.tiempo_actual + tiempo_mant
-                    estado.update({
-                        'Máquina': equipo_libre['id'],
-                        'RND Mantenimiento': round(rnd_mant, 2),
-                        'Tiempo Mantenimiento': round(tiempo_mant, 2),
-                        'Fin Mantenimiento': round(equipo_libre['fin_mantenimiento'], 2)
-                    })
-                self.proximo_mantenimiento += 60
-
-            elif evento[0] in ['Fin Inscripción', 'Fin Mantenimiento']:
-                equipo_id = evento[2]
-                equipo = next(eq for eq in self.equipos if eq['id'] == equipo_id)
-                equipo['estado'] = EstadoEquipo.LIBRE
-                if evento[0] == 'Fin Inscripción':
-                    equipo['fin_inscripcion'] = None
-                    if self.cola > 0:
-                        self.cola -= 1
-                else:
-                    equipo['fin_mantenimiento'] = None
+            else:
+                self.cola += 1
 
             # Actualizar estado de equipos
             for i, equipo in enumerate(self.equipos, 1):
@@ -143,6 +84,38 @@ class Simulacion:
 
             estado['Cola'] = self.cola
             self.resultados.append(estado)
+
+            # Verificar fin de inscripciones
+            for equipo in self.equipos:
+                if (equipo['estado'] == EstadoEquipo.OCUPADO and 
+                    equipo['fin_inscripcion'] and 
+                    equipo['fin_inscripcion'] <= proxima_llegada):
+                    self.resultados.append({
+                        'Evento': f'Fin Inscripción {equipo["alumno_actual"]}',
+                        'Reloj': round(equipo['fin_inscripcion'], 2),
+                        'RND Llegada': 'N/A',
+                        'Tiempo Llegada': 'N/A',
+                        'Próxima Llegada': round(proxima_llegada, 2),
+                        'Máquina': equipo['id'],
+                        'RND Inscripción': 'N/A',
+                        'Tiempo Inscripción': 'N/A',
+                        'Fin Inscripción': round(equipo['fin_inscripcion'], 2),
+                        'RND Mantenimiento': 'N/A',
+                        'Tiempo Mantenimiento': 'N/A',
+                        'Fin Mantenimiento': 'N/A',
+                        'Cola': self.cola
+                    })
+                    equipo['estado'] = EstadoEquipo.LIBRE
+                    equipo['fin_inscripcion'] = None
+                    equipo['alumno_actual'] = None
+                    if self.cola > 0:
+                        self.cola -= 1
+
+            self.tiempo_actual = proxima_llegada
+            rnd_llegada, tiempo_llegada = self.generar_tiempo_llegada()
+            proxima_llegada = self.tiempo_actual + tiempo_llegada
+            self.contador_alumnos += 1
+            id_actual = f"A{self.contador_alumnos}"
 
         return sorted(self.resultados, key=lambda x: x['Reloj'])
 
